09faa2f816e9c43d9b1c217da186e661c9d3d2d4
thomas <thomas@beat.no>
2022-12-08 22:33:31 +0100

    d07: simplyfy and test ifstream

--- src/d07.cpp
+++ src/d07.cpp
@@ -1,113 +1,50 @@
 #include "utils.hpp"
+#include <algorithm>
 #include <cstddef>
 #include <cstdint>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
 #include <numeric>
+#include <sstream>
 #include <string>
+#include <string_view>
 #include <vector>
 
 namespace d07 {
-struct File {
-  std::string name;
-  uint32_t size;
-};
-
-struct Directory {
-  std::string name;
-  Directory *parent;
-  std::vector<Directory> subdirectories;
-  std::vector<File> files;
-};
-
-Directory parse_history(std::vector<std::string> cmds) {
-  Directory root = {"/", nullptr, {}, {}};
-  root.parent = &root;
-  Directory *current_directory = &root;
-
-  for (const auto cmd : cmds) {
-    auto tokens = utils::split(cmd, ' ');
-    if (tokens.at(0) == "$") {
-      if (tokens.at(1) == "cd") {
-        if (tokens.at(2) == "..") {
-          current_directory = current_directory->parent;
-        } else if (tokens.at(2) == "/") {
-          current_directory = &root;
-        } else {
-          for (auto &subdirectory : current_directory->subdirectories) {
-            if (subdirectory.name == tokens.at(2)) {
-              current_directory = &subdirectory;
-              break;
-            }
-          }
-        }
-      }
-    }
-
-    else if (tokens.at(0) == "dir") {
-      Directory new_directory = {tokens.at(1), current_directory, {}, {}};
-      current_directory->subdirectories.push_back(new_directory);
-    } else {
-      uint32_t file_size = utils::to_uint32(tokens.at(0));
-      File new_file = {tokens.at(1), file_size};
-      current_directory->files.push_back(new_file);
-    }
-  }
-
-  return root;
-}
-
-uint32_t fold_directories(const Directory &dir, uint32_t &acc) {
-  uint32_t this_dir_size = 0;
-
-  this_dir_size += std::accumulate(
-      dir.files.begin(), dir.files.end(), 0,
-      [](auto a, const auto &f) { return a += f.size; });
-
-  this_dir_size += std::accumulate(
-      dir.subdirectories.begin(), dir.subdirectories.end(), 0,
-      [&acc](auto a, const auto &d) { return a += fold_directories(d, acc); });
-
-  // conditionally tally
-  if (this_dir_size < 100000) {
-    acc += this_dir_size;
-  }
-
-  return this_dir_size;
-}
-
-const uint32_t part_one(std::vector<std::string> cmds) {
-  auto root = parse_history(cmds);
+uint32_t parse_size(std::ifstream &cmds, std::vector<uint32_t> &sizes) {
   uint32_t acc = 0;
-  fold_directories(root, acc);
-
+  std::vector<std::string> tokens;
+  for (std::string cmd; std::getline(cmds, cmd) && cmd != "$ cd ..";) {
+    if (std::isdigit(cmd[0])) {
+      tokens = utils::split(cmd, ' ');
+      acc += utils::to_uint32(tokens.at(0));
+    } else if (cmd.starts_with("$ cd")) {
+      acc += parse_size(cmds, sizes);
+    }
+  }
+  sizes.push_back(acc);
   return acc;
 }
 
-// TODO
-uint32_t aproximate_directories_part_two(const Directory &dir, uint32_t &approx) {
-  uint32_t this_dir_size = 0;
+const uint32_t part_one(std::ifstream &cmds) {
+  std::vector<uint32_t> sizes;
+  parse_size(cmds, sizes);
 
-  this_dir_size += std::accumulate(
-      dir.files.begin(), dir.files.end(), 0,
-      [](auto a, const auto &f) { return a += f.size; });
-
-  this_dir_size += std::accumulate(
-      dir.subdirectories.begin(), dir.subdirectories.end(), 0,
-      [&approx](auto a, const auto &d) { return a += fold_directories(d, approx); });
-
-  // conditionally tally
-  if (this_dir_size < 100000) {
-    approx += this_dir_size;
-  }
-
-  return this_dir_size;
+  return std::accumulate(sizes.begin(), sizes.end(), 0,
+      [](uint32_t acc, uint32_t size){ if (size < 100'000){ return acc += size; } else { return acc; } });
 }
 
+const uint32_t part_two(std::ifstream &cmds) {
+  uint32_t min_delete = 0;
+  std::vector<uint32_t> sizes;
+  uint32_t all_files = parse_size(cmds, sizes);
 
+  uint32_t TOTAL_SIZE = 70'000'000;
+  uint32_t WANTED_SIZE = 30'000'000;
+  uint32_t needed_min_size = WANTED_SIZE - (TOTAL_SIZE - all_files);
 
-const uint32_t part_two(std::vector<std::string> cmds) {
-  auto root = parse_history(cmds);
-  auto total_space = 70000000;
-  auto needed_space = 30000000;
-
+  std::sort(sizes.begin(), sizes.end());
+  return *std::ranges::lower_bound(sizes, needed_min_size);
 }
 } // namespace d07
--- test/07test.cpp
+++ test/07test.cpp
@@ -1,49 +1,20 @@
 #include "utils.hpp"
 #include <catch2/catch_test_macros.hpp>
 #include <d07.cpp>
+#include <fstream>
 
-const std::vector<std::string> test_input =
-{ "$ cd /",
-  "$ ls",
-  "dir a",
-  "14848514 b.txt",
-  "8504156 c.dat",
-  "dir d",
-  "$ cd a",
-  "$ ls",
-  "dir e",
-  "29116 f",
-  "2557 g",
-  "62596 h.lst",
-  "$ cd e",
-  "$ ls",
-  "584 i",
-  "$ cd ..",
-  "$ cd ..",
-  "$ cd d",
-  "$ ls",
-  "4060174 j",
-  "8033020 d.log",
-  "5626152 d.ext",
-  "7214296 k" };
-
-
-
-const auto raw_input = utils::load_input("d07input.txt");
-const auto input = utils::split(raw_input, '\n');
+std::string inputfile = "d07input.txt";
 
 TEST_CASE("Day 07-1", "[part 1]") {
-  auto test = d07::part_one(::test_input);
-  auto answer = d07::part_one(::input);
+  std::ifstream input(::inputfile.c_str());
+  auto answer = d07::part_one(input);
 
-  REQUIRE(95437 == test);
   REQUIRE(1845346 == answer);
 }
 
 TEST_CASE("Day 07-2", "[part 2]") {
-  auto test = d07::part_two(::test_input);
-//  auto answer = d07::part_two(::input);
+  std::ifstream input(::inputfile.c_str());
+  auto answer = d07::part_two(input);
 
-  REQUIRE(24933642 == test);
-//  REQUIRE(1845346 == answer);
+  REQUIRE(3636703 == answer);
 }
